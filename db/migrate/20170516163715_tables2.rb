3321a89cc3af7b4da8061f74344583bf0eddbb91
Jin Camou and Sara Schultz <jin@>
2017-05-16 11:38:20 -0700

    delete all rakefiles from db branch

--- db/migrate/20170516163715_tables2.rb
+++ db/migrate/20170516163715_tables2.rb
@@ -1,15 +1,15 @@
-class Tables2 < ActiveRecord::Migration[5.1]
-  def change
-    create_table(:world_ufos) do |t|
-      t.column(:sight_date, :text)
-      t.column(:post_date, :text)
-      t.column(:city_state, :text)
-      t.column(:duration, :text)
-      t.column(:summary, :text)
-      t.column(:latitude, :text)
-      t.column(:longitude, :text)
-      # t.column(:junk, :text)
-      # t.column(:junk1, :text)
-    end
-  end
-end
+# class Tables2 < ActiveRecord::Migration[5.1]
+#   def change
+#     create_table(:world_ufos) do |t|
+#       t.column(:sight_date, :text)
+#       t.column(:post_date, :text)
+#       t.column(:city_state, :text)
+#       t.column(:duration, :text)
+#       t.column(:summary, :text)
+#       t.column(:latitude, :text)
+#       t.column(:longitude, :text)
+#       # t.column(:junk, :text)
+#       # t.column(:junk1, :text)
+#     end
+#   end
+# end
