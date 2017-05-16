d5d01190f16750c4b3f41d25b16ae638a36f8d3f
Dominic Brown and Sara Schultz <dominic@>
2017-05-15 18:27:06 -0700

    add js for printing map + markers to index view

--- Gemfile
+++ Gemfile
@@ -3,11 +3,13 @@ source "https://rubygems.org"
 
 gem "sinatra-contrib", require: "sinatra/reloader"
 gem "sinatra-activerecord"
+gem "gon-sinatra"
 gem "rake"
 gem "pg"
 gem "sinatra"
 # gem "google_maps_service"
 gem "static-gmaps"
+gem "json"
 
 group :test do
   gem "rspec"
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,29 +1,51 @@
 
-// OREGON
+//// this initMap is a 'callback' fucntion attached to the end of the URL on index.erb
 function initMap() {
-  var oregon = {lat: 43.8, lng: -120.5};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  // Oregon cities only
+  var Cities = {
+    adair: {lat: 44.67, lng: -123.22},
+    adrian: {lat: 43.74, lng: -117.07},
+    albany: {lat: 44.63, lng: -123.10},
+    aloha: {lat: 45.50, lng: -122.87},
+    alsea: {lat: 44.38, lng: -123.60},
+    alvadore: {lat: 44.13, lng: -123.26},
+    alvord: {lat: 42.54, lng: -118.46},
+    amity: {lat: 45.12, lng: -123.21},
+    beaverton: {lat: 45.49, lng: -122.80},
+    bend: {lat: 44.06, lng: -121.32},
+    eugene: {lat: 44.05, lng: -123.08},
+    hillsboro: {lat: 45.52, lng: -122.99},
+    portland: {lat: 45.52, lng: -122.68},
+    albertLake: {lat: 43.45, lng: -119.14},
+    alfalfa: {lat: 44.08, lng: -121.01}
+    //   : {lat: , lng: };
+  };
+
+  // creates a new google maps object and centers on a area
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
-    center: oregon
+    center: {lat: 44.06, lng: -121.32}
   });
 
-  // var marker = new google.maps.Marker({
-  //   position: portland,
-  //   map: map
-  // });
+  // adds the coordinates for each city into an array
+  coords = [];
+  for (var key in Cities) {
+    console.log("key:", Cities[key])
+    coords.push(Cities[key]);
+  };
 
-}
+  // places each marker for the city
+  for (var city in coords) {
+    coord = coords[city];
+    marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+  }
 
-// AUSTRALIA
-// function initMap() {
-//   var uluru = {lat: -25.363, lng: 131.044};
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 4,
-//     center: uluru
-//   });
-//   var marker = new google.maps.Marker({
-//     position: uluru,
-//     map: map
-//   });
-// }
+} //// END INITMAP
+
+
+//  injections = <%= @injections.to_json %>;
+//  medicines = <%= @medicines.to_json %>;
