b3f92e7b88562e3eb74f3618d282b38f8f901950
Dominic Brown and jin camou <dominic+jin@>
2017-05-17 15:54:05 -0700

    add styles, simplify search, simplify reports

--- lib/ufo.rb
+++ lib/ufo.rb
@@ -11,28 +11,44 @@ end
 
 
 module Info
+  @@city = ""
   @@lat = 0
   @@lng = 0
   @@reports = []
+  @@total = 0
 
-  def Info.get_lat
-    @@lat
+  def Info.save_city(city)
+    @@city = city
   end
-  def Info.get_lng
-    @@lng
+  def Info.get_city
+    @@city
   end
+
   def Info.save_lat(num)
     @@lat = num
   end
+  def Info.get_lat
+    @@lat
+  end
   def Info.save_lng(num)
     @@lng = num
+  end
+  def Info.get_lng
+    @@lng
   end
 
   def Info.save_rep(arr)
     @@reports = arr
   end
-  def Info.get_rep(arr)
-    @@reports = arr
+  def Info.get_rep
+    @@reports
+  end
+
+  def Info.save_total(num)
+    @@total = num
+  end
+  def Info.get_total
+    @@total
   end
 
 end
