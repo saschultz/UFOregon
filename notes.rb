65edc04c2c9d2c72aa82756f9f4e0ee7bbd954cc
Dominic Brown and jin camou <jin@>
2017-05-17 10:32:31 -0700

    refactor routes, add Info class, update map it function

--- notes.rb
+++ notes.rb
@@ -29,4 +29,4 @@ rake db:create
 rake db:migrate
 rake db:test:prepare
 ##in psql \c to oregon_sightings_development:
-COPY ufos(sight_date, city, state, shape, duration, summary, latitude, longitude) FROM '/Users/USERNAME/Desktop/UFOregon/raw_data/oregon_sightings_latlong.csv' DELIMITER ',' CSV;
+COPY ufos(sight_date, city, state, shape, duration, summary, latitude, longitude) FROM '/Users/Guest/Desktop/UFOregon/raw_data/oregon_sightings_latlong.csv' DELIMITER ',' CSV;
