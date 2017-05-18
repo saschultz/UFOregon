29d35657c27d283589ca6ffad5042e2f0b3bffa2
Dominic Brown and Dana Weiss <dana@>
2017-05-18 15:03:29 -0700

    refactor, NOT READY FOR PRODUCTION

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -88,23 +88,31 @@ var google_styles = [
   }
 ];
 
-//////////////////v////////////////////////////////////
+///////////////////////////////////////////////////////
 //// initMap = master google maps API output
 //////////////////////////////////////////////////////
 function initMap(queryData) {
   // console.log("queryData: " , queryData);
+
+
   let startCoord = new google.maps.LatLng(queryData[0]["lat"],queryData[0]["lng"]);
   // creates a new google maps object and centers on a area
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
-    // center: {lat: 44.06, lng: -121.32},
     center: startCoord,
     styles: google_styles
   });
   // places each marker for the city
+  var myContent = "";
   var ufo = './img/ufo_marker_eerie.png'
+  if (queryData.length < 1) {
+    queryData.shift()
+  }
+
+  console.log("queryData: " , queryData);
+
   queryData.forEach(function(hash_obj) {
-    let coord = new google.maps.LatLng(hash_obj['lat'],hash_obj['lng']);
+    var coord = new google.maps.LatLng(hash_obj['lat'],hash_obj['lng']);
     marker = new google.maps.Marker({
       position: coord,
       animation: google.maps.Animation.DROP,
@@ -113,65 +121,59 @@ function initMap(queryData) {
     });
     marker.addListener('click', toggleBounce);
     marker.addListener('click', showReport);
+    setting up the marker window HTML content
+    if (hash_obj['tot'] !== 0) {
+      myContent = "<h3 class='blk_text'>" + hash_obj["nam"] + "</h3><br>" + "<p class='blk_text'>Found = " + hash_obj["tot"] + " UFOs <br>" +
+      "<p class='blk_text'>Lat= " + hash_obj["lat"] + "<br>" + " Long= " + hash_obj["lng"] + "<p>";
+    }
+    function toggleBounce() {
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+    }
+
+    var infowindow = new google.maps.InfoWindow({
+      content: myContent
+    });
   });
+
+  // google.maps.event.addListener(marker, 'click', function() {
+  //   infowindow.open(map, marker, queryData);
+  // });
+  // function showReport() {
+  //   $('#scrollable-content').show();
+  // }
 
   // setting up the marker window HTML content
-  var myContent = "";
-  if (queryData[0]['tot'] !== 0) {
-    myContent = "<h3 class='blk_text'>" + queryData[0]["nam"] + "</h3><br>" + "<p class='blk_text'>Found = " + queryData[0]["tot"] + " UFOs <br>" +
-    "<p class='blk_text'>Lat= " + queryData[0]["lat"] + "<br>" + " Long= " + queryData[0]["lng"] + "<p>";
-  }
-  // map marker window
+  // var myContent = "";
+  // if (queryData[0]['tot'] !== 0) {
+  //   myContent = "<h3 class='blk_text'>" + queryData[0]["nam"] + "</h3><br>" + "<p class='blk_text'>Found = " + queryData[0]["tot"] + " UFOs <br>" +
+  //   "<p class='blk_text'>Lat= " + queryData[0]["lat"] + "<br>" + " Long= " + queryData[0]["lng"] + "<p>";
+  // }
+} ////// END initMap
 
 
-  function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-      marker.setAnimation(null);
-    } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
 
-  function showReport() {
-    $('#scrollable-content').show();
-  }
-
-  // You can use a LatLng literal in place of a google.maps.LatLng object when
-  // creating the Marker object. Once the Marker object is instantiated, its
-  // position will be available as a google.maps.LatLng object. In this case,
-  // we retrieve the marker's position using the
-  // google.maps.LatLng.getPosition() method.
-
-  var infowindow = new google.maps.InfoWindow({
-    content: myContent
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map, marker, queryData);
-  });
-}
 
 ///////////////////
 // FRONT END
 ///////////////////
 $(document).ready(function() {
 
-  test_hash = $("#json_test1").text();
-
-  $("#toggle_map").click(function() {
-    $("#map_super").toggleClass("hide");
-    console.log("toggle_map clicked");
-  });
-
-  $("#all_namies").click(function() {
-    console.log("all_cities clicked");
-  });
+  // test_hash = $("#json_test1").text();
 
   // AJAX get request - to refresh the map
   $.get("/ruby_data", function(ruby_data) {
     console.log("AJAX $.get('/ruby_data' happened");
     parsed_data = JSON.parse(ruby_data);
     initMap(parsed_data);
+  });
+
+  $("#toggle_map").click(function() {
+    $("#map_super").toggleClass("hide");
+    console.log("toggle_map clicked");
   });
 
 });
